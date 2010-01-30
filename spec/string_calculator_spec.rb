0bb4bb888400cb599c90ec15a79ce020e3c2b8b3
Kerry Buckley <kerryjbuckley@gmail.com>
2010-01-30 10:51:03 +0000

    Change name of matcher to evaluate_to

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,7 +3,7 @@ require "spec"
 $:.unshift File.expand_path("../lib", File.dirname(__FILE__))
 require "string_calculator"
 
-Spec::Matchers.define :be_calculated_as do |expected|
+Spec::Matchers.define :evaluate_to do |expected|
   match do |input|
     @result = StringCalculator.calculate(input)
     @result.should == expected
@@ -17,27 +17,27 @@ end
 describe StringCalculator do
   describe "with comma-separated numbers" do
     it "calculates the sum of an empty string as 0" do
-      "".should be_calculated_as(0)
+      "".should evaluate_to(0)
     end
 
     it "calculates the sum of one number as the number" do
-      "1".should be_calculated_as(1)
+      "1".should evaluate_to(1)
     end
 
     it "calculates the sum of two numbers" do
-      "1,2".should be_calculated_as(3)
+      "1,2".should evaluate_to(3)
     end
 
     it "calculates the sum of five numbers" do
-      "1,2,3,4,5".should be_calculated_as(15)
+      "1,2,3,4,5".should evaluate_to(15)
     end
   end
 
   it "allows a newline delimiter" do
-    "1\n2\n3".should be_calculated_as(6)
+    "1\n2\n3".should evaluate_to(6)
   end
 
   it "allows a mixture of comma and newline delimiters" do
-    "1,2\n3".should be_calculated_as(6)
+    "1,2\n3".should evaluate_to(6)
   end
 end
