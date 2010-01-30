76be129e5d7b0bef78e64093c484bb054267eddd
Kerry Buckley <kerryjbuckley@gmail.com>
2010-01-30 20:06:12 +0000

    Oops, those methods were supposed to be in the class!

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,36 +2,38 @@ class StringCalculator
   def self.calculate input
     sum(extract_numbers(values(input), delimiter_pattern(input)))
   end
-end
 
-private
+  class << self
+    private
 
-def delimiter_pattern input
-  case input
-  when %r(\A//((\[.+\])+)$) then %r{(#{$1[1..-2].split("][").map{|str| Regexp.escape(str)}.join("|")})}
-  when %r(\A//(.+)$) then $1
-  else %r([,\n])
+    def delimiter_pattern input
+      case input
+      when %r(\A//((\[.+\])+)$) then %r{(#{$1[1..-2].split("][").map{|str| Regexp.escape(str)}.join("|")})}
+      when %r(\A//(.+)$) then $1
+      else %r([,\n])
+      end
+    end
+
+    def values input
+      input =~ %r((\A//.+?)$) ? input.sub($1, '') : input
+    end
+
+    def extract_numbers input, delimiter
+      input.split(delimiter).map(&:to_i)
+    end
+
+    def sum numbers
+      reject_negatives numbers
+      ignore_over_1000(numbers).inject(0, &:+)
+    end
+
+    def reject_negatives numbers
+      negatives = numbers.select {|n| n < 0}
+      raise "Negatives not allowed: #{negatives.join(", ")}" unless negatives.empty?
+    end
+
+    def ignore_over_1000 numbers
+      numbers.reject {|n| n > 1000}
+    end
   end
-end
-
-def values input
-  input =~ %r((\A//.+?)$) ? input.sub($1, '') : input
-end
-
-def extract_numbers input, delimiter
-  input.split(delimiter).map(&:to_i)
-end
-
-def sum numbers
-  reject_negatives numbers
-  ignore_over_1000(numbers).inject(0, &:+)
-end
-
-def reject_negatives numbers
-  negatives = numbers.select {|n| n < 0}
-  raise "Negatives not allowed: #{negatives.join(", ")}" unless negatives.empty?
-end
-
-def ignore_over_1000 numbers
-  numbers.reject {|n| n > 1000}
 end
